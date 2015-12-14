3e5b2bfe77379cc1317c0d757148a0f12e61deac
Dan Bate <dan@wildfish.com>
2015-12-14 16:45:36 +0000

    added pytest config

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autocomplete_search',
 
-    'demo.app'
+    'demo.app',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ tests/test_search_view_autocomplete.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+
+class SearchViewAutocomplte(TestCase):
+    def test_foo___bar(self):
+        pass
